fd3e7e8e7b32d4d291a6c0fdd7cbf8c5d14c2af6
Neophos <neophos@destinationfight.com>
2021-08-29 22:13:04 +0200

    .

---
+++ src/card.rs
@@ -0,0 +1,4 @@
+pub mod card;
+
+power: i32;
+
---
+++ src/main.rs
@@ -0,0 +1,38 @@
+#![allow(non_snake_case)]
+
+#[macro_use] extern crate rocket;
+
+use card;
+
+#[get("/")]
+fn game() -> String {
+    format!(":<!")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![game])
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rocket::local::blocking::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn shouldSuccessStatus() {
+        let client = Client::tracked(rocket()).expect("rocketo uwu");
+        let response = client.get("/").dispatch();
+
+        assert_eq!(response.status(), Status::Ok);
+    }
+
+    #[test]
+    fn shouldHappyFace() {
+        let client = Client::tracked(rocket()).expect("rocketo uwu");
+        let response = client.get("/").dispatch();
+
+        assert_eq!(response.into_string().unwrap(), ":<!");
+    }
+}
